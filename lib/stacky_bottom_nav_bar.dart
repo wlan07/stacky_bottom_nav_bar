45d4b55846f75285e1fdf63b8239cea8905118f2
wlan07 <albertoalberto58940053@gmail.com>
2022-03-29 01:42:15 +0100

    critical hotfix 🚑️

--- lib/stacky_bottom_nav_bar.dart
+++ lib/stacky_bottom_nav_bar.dart
@@ -1,3 +1,6 @@
 library stacky_bottom_nav_bar;
 
 export 'src/stacky_bottom_nav_bar.dart';
+export 'src/models/stacky_animated_nav_bar_item.dart';
+export 'src/models/stacky_bottom_nav_bar_params.dart';
+export 'src/models/stacky_simple_nav_bar_item.dart';
